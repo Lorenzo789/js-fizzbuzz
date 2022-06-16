c08498d9295730aa7b6f3e463ac918d68856cfbc
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-16 17:17:25 +0200

    bonus 2 almost done

--- js/myscript.js
+++ js/myscript.js
@@ -5,9 +5,17 @@ const buzzFizzRow = document.getElementById('number-row');
 //CICLE FOR
 for (let index = 1; index <= 100; index++) {
 
-    const boxNumber = document.createElement('div');
+    //CREATE VARIABLE
+    let boxNumber = document.createElement('div');
+
+    //ADD CLASS
+    boxNumber.classList.add('ms_box_number');
+
+    //CHANGE INNER TEXT
     boxNumber.innerText = index;
-    //buzzFizzRow.append(boxNumber);
+
+    //INSERT boxNumber 
+    buzzFizzRow.append(boxNumber);
 
 
 
@@ -17,14 +25,23 @@ for (let index = 1; index <= 100; index++) {
     if ((index % 5 == 0) && (index % 3 == 0)) {
 
         console.log('fizzbuzzz');
+        boxNumber.classList.add('ms_box_number');
+        boxNumber.innerText = 'fizzbuzz';
+        buzzFizzRow.append(boxNumber);
 
     } else if (index % 5 == 0) {
 
         console.log('buzz');
+        boxNumber.classList.add('ms_box_number');
+        boxNumber.innerText = 'buzz';
+        buzzFizzRow.append(boxNumber);
 
     } else if (index % 3 == 0) {
 
         console.log('fizz');
+        boxNumber.classList.add('ms_box_number');
+        boxNumber.innerText = 'fizz';
+        buzzFizzRow.append(boxNumber);
 
     } else {
 
