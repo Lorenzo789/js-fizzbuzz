14c8cf1848252b6c2ac72a1a8c1ca3499c81ddb1
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-16 16:39:27 +0200

    comments working on bonus 1

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,19 @@
 
 
+const buzzFizzRow = document.getElementById('number-row');
+
+//CICLE FOR
 for (let index = 1; index <= 100; index++) {
 
+    const boxNumber = document.createElement('div');
+
+
+
+
+
+
+
+    //CONDITION
     if ((index % 5 == 0) && (index % 3 == 0)) {
 
         console.log('fizzbuzzz');
